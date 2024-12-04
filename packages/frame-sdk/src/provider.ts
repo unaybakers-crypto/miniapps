e716b1cc7cad3ec1034271e3ffbfad86908183c2
Tony D'Addeo <tony@merklemanufactory.com>
2024-12-04 14:44:53 -0600

    docs(changeset): Support Ethereum Provider events, preserve errors over the wire

--- packages/frame-sdk/src/provider.ts
+++ packages/frame-sdk/src/provider.ts
@@ -1,26 +1,75 @@
-import { Provider, RpcRequest } from "ox";
+import { Provider, RpcRequest, RpcResponse } from "ox";
 import { frameHost } from "./frameHost";
+import { EthProviderWireEvent } from "@farcaster/frame-core";
 
 const emitter = Provider.createEmitter();
 const store = RpcRequest.createStore();
 
-export const provider = Provider.from({
+type GenericProviderRpcError = {
+  code: number;
+  details?: string;
+}
+
+export function toProviderRpcError({ code, details }: GenericProviderRpcError): Provider.ProviderRpcError {
+  switch (code) {
+    case 4001:
+      return new Provider.UserRejectedRequestError();
+    case 4100:
+      return new Provider.UnauthorizedError();
+    case 4200:
+      return new Provider.UnsupportedMethodError();
+    case 4900:
+      return new Provider.DisconnectedError();
+    case 4901:
+      return new Provider.ChainDisconnectedError();
+    default:
+      return new Provider.ProviderRpcError(code, details ?? 'Unknown provider RPC error');
+  }
+}
+
+export const provider: Provider.Provider = Provider.from({
   ...emitter,
   async request(args) {
-    return await frameHost.ethProviderRequest(
-      // @ts-expect-error - from ox examples but our FetchFn needs better typing
-      store.prepare(args),
-    );
-  },
+    // @ts-expect-error
+    const request = store.prepare(args);
+
+    try {
+      const response = await frameHost.ethProviderRequestV2(
+        request
+      ).then((res) => RpcResponse.parse(res, { request, raw: true }));
+
+
+      if (response.error) {
+        throw toProviderRpcError(response.error)
+      }
+
+      return response.result;
+    } catch (e) {
+      // ethProviderRequestV2 not supported, fall back to v1
+      if (e instanceof Error && e.message.match(/cannot read property 'apply'/i)) {
+        return await frameHost.ethProviderRequest(request);
+      }
+
+      if (
+        e instanceof Provider.ProviderRpcError ||
+        e instanceof RpcResponse.BaseError
+      ) {
+        throw e;
+      }
+
+
+      throw new RpcResponse.InternalError({ message: e instanceof Error ? e.message : undefined })
+    }
+  }
 });
 
-export type ProviderType = typeof provider;
-
 // Required to pass SSR
-if (typeof document !== 'undefined')
-  document.addEventListener("FarcasterFrameEvent", (event) => {
+if (typeof document !== 'undefined') {
+  document.addEventListener("FarcasterFrameEthProviderEvent", (event) => {
     if (event instanceof MessageEvent) {
-      // TODO narrow to EventMap types and emit
-      // emitter.emit(event.type as (keyof Provider.EventMap), event.data);
+      const ethProviderEvent = event.data as EthProviderWireEvent;
+      // @ts-expect-error 
+      emitter.emit(ethProviderEvent.event, ...ethProviderEvent.params);
     }
   });
+}
