e1ee6fc68e09e6275800425c46334599f81ae0d6
Tony D'Addeo <tony@merklemanufactory.com>
2024-12-16 15:24:48 -0600

    accept next as a valid domain frame schema version

--- packages/frame-core/src/schemas/manifest.ts
+++ packages/frame-core/src/schemas/manifest.ts
@@ -9,8 +9,14 @@ import {
 
 export const domainFrameConfigSchema = z.object({
   // 0.0.0 and 0.0.1 are not technically part of the spec but kept for
-  // backwards compatibilty
-  version: z.union([z.literal("0.0.0"), z.literal("0.0.1"), z.literal("1")]),
+  // backwards compatibilty. next should always resolve to the most recent
+  // schema version.
+  version: z.union([
+    z.literal("0.0.0"), 
+    z.literal("0.0.1"), 
+    z.literal("1"), 
+    z.literal('next')
+  ]),
   name: frameNameSchema,
   iconUrl: secureUrlSchema,
   homeUrl: secureUrlSchema,
