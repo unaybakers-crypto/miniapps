f52b814747ecadd3109b37396de7c5d23a8a472c
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-12-22 14:27:04 +0200

    feat: createVerifyAppKeyWithHub

---
+++ packages/frame-node/tests/farcaster.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { createVerifyAppKeyWithHub } from "../src/farcaster";
+
+test("farcaster", async () => {
+  const hubUrl = "https://nemes.farcaster.xyz:2281";
+  const verifyAppKey = createVerifyAppKeyWithHub(hubUrl);
+  const result = await verifyAppKey(
+    5448,
+    "0x5d4cd906de103d6fff5a50869164344827d338b78d887cb7d5d1ca1cf62711ee"
+  );
+
+  expect(result).toMatchObject({ valid: true, appFid: 9152 });
+});
