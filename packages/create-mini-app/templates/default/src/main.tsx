7d1db117bb4bdfab5fab7845d0d7867d7921fecd
Tony D'Addeo <tony@merklemanufactory.com>
2025-03-25 17:31:19 -0500

    [create-mini-app] fix lint warnings (#141)
    * fix lint warnings
    
    * docs(changeset): fixed lint warnings
    
    * RELEASING: Releasing 1 package(s)
    
    Releases:
      @farcaster/create-mini-app@0.0.6

--- packages/create-mini-app/templates/default/src/main.tsx
+++ packages/create-mini-app/templates/default/src/main.tsx
@@ -10,7 +10,7 @@ import './index.css'
 
 const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
