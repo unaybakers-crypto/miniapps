59f7566c47c66f3314b40c5e18348fb7f6bd1cd4
Tony D'Addeo <tony@merklemanufactory.com>
2025-03-25 16:27:02 -0500

    remove unneeded buffer polyfill

--- packages/create-mini-app/templates/default/src/main.tsx
+++ packages/create-mini-app/templates/default/src/main.tsx
@@ -1,19 +1,16 @@
-import { Buffer } from 'node:buffer'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { WagmiProvider } from "wagmi";
 
-import App from './App.tsx'
-import { config } from './wagmi.ts'
+import App from "./App.tsx";
+import { config } from "./wagmi.ts";
 
-import './index.css'
+import "./index.css";
 
-globalThis.Buffer = Buffer
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
@@ -21,4 +18,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>,
-)
+);
